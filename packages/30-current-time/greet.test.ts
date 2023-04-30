1c53de41da97d4ca5751a7df7c22a1d7799d25ff
tolluset <dlqud19@gmail.com>
2023-04-30 21:34:01 +0900

    feat: add greet

--- packages/30-current-time/greet.test.ts
+++ packages/30-current-time/greet.test.ts
@@ -1,7 +1,51 @@
-import { test, expect } from 'vitest'
-import { greet } from './greet'
+import { describe, it, expect, vi } from 'vitest'
+import { greet, nativeGreet } from './greet'
 
-test('現在時刻が12時から18時未満の場合、「こんにちは」と挨拶する', () => {
-  const result = greet()
-  expect(result).toBe('こんにちは')
+describe('greet', () => {
+  it('現在時刻が5時から12時未満の場合、「おはようございます」と挨拶する', () => {
+    const now = {
+      getHours: () => 5,
+    } as Date
+
+    const result = greet(now)
+    expect(result).toBe('おはようございます')
+  })
+
+  it('現在時刻が12時から18時未満の場合、「こんにちは」と挨拶する', () => {
+    const now = {
+      getHours: () => 12,
+    } as Date
+
+    const result = greet(now)
+    expect(result).toBe('こんにちは')
+  })
+
+  it('現在時刻が18時から5時未満の場合、「こんばんは」と挨拶する', () => {
+    const now = {
+      getHours: () => 18,
+    } as Date
+
+    const result = greet(now)
+    expect(result).toBe('こんばんは')
+  })
+})
+
+describe('nativeGreet', () => {
+  it('現在時刻が5時から12時未満の場合、「おはようございます」と挨拶する', () => {
+    vi.useFakeTimers().setSystemTime(new Date('2023-01-01 05:00:00'))
+    const result = nativeGreet()
+    expect(result).toBe('おはようございます')
+  })
+
+  it('現在時刻が12時から18時未満の場合、「こんにちは」と挨拶する', () => {
+    vi.useFakeTimers().setSystemTime(new Date('2023-01-01 12:00:00'))
+    const result = nativeGreet()
+    expect(result).toBe('こんにちは')
+  })
+
+  it('現在時刻が18時から5時未満の場合、「こんばんは」と挨拶する', () => {
+    vi.useFakeTimers().setSystemTime(new Date('2023-01-01 18:00:00'))
+    const result = nativeGreet()
+    expect(result).toBe('こんばんは')
+  })
 })
--- packages/30-current-time/greet.ts
+++ packages/30-current-time/greet.ts
@@ -1,5 +1,22 @@
 /* eslint-disable yoda */
-export const greet = (): 'おはようございます' | 'こんにちは' | 'こんばんは' => {
+export const greet = (
+  now: Date
+): 'おはようございます' | 'こんにちは' | 'こんばんは' => {
+  if (5 <= now.getHours() && now.getHours() < 12) {
+    return 'おはようございます'
+  }
+
+  if (12 <= now.getHours() && now.getHours() < 18) {
+    return 'こんにちは'
+  }
+
+  return 'こんばんは'
+}
+
+export const nativeGreet = ():
+  | 'おはようございます'
+  | 'こんにちは'
+  | 'こんばんは' => {
   const now = new Date()
 
   if (5 <= now.getHours() && now.getHours() < 12) {
