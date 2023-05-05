adebb45ba6f49f41c97630fa3c3c5b765e4ca862
tolluset <dlqud19@gmail.com>
2023-05-06 00:10:42 +0900

    feat: 30-db

--- packages/30-db/fetch-product-price-with-tax.test.ts
+++ packages/30-db/fetch-product-price-with-tax.test.ts
@@ -1,7 +1,18 @@
 import { expect, test } from 'vitest'
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb'
 import { fetchProductPriceWithTax } from './fetch-product-price-with-tax'
 
+const ddbDoc = {
+  get: async () => ({
+    Item: {
+      no: '0001',
+      name: 'トイレ消臭剤',
+      price: 100,
+    },
+  }),
+} as unknown as DynamoDBDocument
+
 test('商品Noから消費税込みの値段を取得する', async () => {
-  const result = await fetchProductPriceWithTax('0001')
-  expect(result).toBe(110)
+  const result = await fetchProductPriceWithTax('0001', ddbDoc)
+  expect(result).toBeCloseTo(110)
 })
